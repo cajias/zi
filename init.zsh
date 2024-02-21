80f95f51c6a96352d2ec11f43104caf17eb1e5b7
Raul <81768037+cajias@users.noreply.github.com>
2024-02-21 17:38:11 +0100

    Update init.zsh

--- init.zsh
+++ init.zsh
@@ -49,6 +49,7 @@ zinit wait lucid light-mode for \
 
 zinit snippet OMZP::git-prompt
 zinit snippet OMZP::colored-man-pages
+zinit snippet OMZP::dotenv
 zinit snippet OMZP::colorize
 zinit snippet OMZL::git.zsh
 zinit snippet OMZP::git
@@ -65,6 +66,3 @@ zstyle :omz:plugins:ssh-agent helper ksshaskpass
 zstyle :omz:plugins:ssh-agent ssh-add-args --apple-load-keychain
 
 zinit ice depth=1; zinit light romkatv/powerlevel10k
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
